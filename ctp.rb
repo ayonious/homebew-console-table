b8dbda4056206d33ed2f38fe6b41a78d8cfcb4f6
Nahiyan Kamal <noyanayihan@gmail.com>
2025-05-27 23:49:19 +0200

    test message

--- ctp.rb
+++ ctp.rb
@@ -11,7 +11,7 @@ class Ctp < Formula
   
     def install
       system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-      bin.install_symlink Dir["#{HOMEBREW_PREFIX}/lib/node_modules/table-printer-cli/bin/*"]
+      bin.install_symlink Dir["#{libexec}/bin/*"]
     end
   
     test do
