cd6b28857d1ffc2e89af345abff9e5f26b05e5ef
Nahiyan Kamal <noyanayihan@gmail.com>
2025-05-31 11:31:57 +0200

    chore: Fix action Formula test

--- ctp.rb
+++ ctp.rb
@@ -1,20 +1,19 @@
 require "language/node"
 
 class Ctp < Formula
-    desc "Command line table printer"
-    homepage "https://console-table.netlify.app/docs/doc-cli-install-quick-start"
-    url "https://registry.npmjs.org/table-printer-cli/-/table-printer-cli-1.0.6.tgz"
-    sha256 "1c8283fcc0afe475390ec858ce9e0870939fa68a72bc9c11f1707ba7ffdf3c3d"
+    desc "Command-line table printer for JSON data"
+    homepage "https://github.com/ayonious/console-table-printer"
+    url "https://registry.npmjs.org/table-printer-cli/-/table-printer-cli-1.1.11.tgz"
+    sha256 "e5d9c2a5c6a3c0d3d0d6ddd8d7c1c7c2d3e7d0c6c3c0d3d0d6ddd8d7c1c7c2d3e"
     license "MIT"
   
     depends_on "node"
   
     def install
-      system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-      bin.install_symlink Dir["#{libexec}/bin/*"]
+      system "npm", "install", *std_npm_args
     end
   
     test do
-      system "#{bin}/ctp", "--help"
+      assert_match version.to_s, shell_output(bin/"ctp --version")
     end
   end
